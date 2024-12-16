f8135d94a26e444cfe28fe1e828af77a52d6e4fb
Michael-Gamal <michealgamal242@gmail.com>
2024-12-17 00:59:43 +0200

    V3

--- src/components/about/AboutCard.jsx
+++ src/components/about/AboutCard.jsx
@@ -3,13 +3,15 @@ import Title from '../common/title/Title.jsx';
 import { homeAbout } from '../../dummydata.js'
 import AWrapper from './AWrapper.jsx'
 import './about.css';
+import IMGAbout from '../../images/about.webp'
 const AboutCard = () => {
   return (
     <>
         <section className="aboutHome">
             <div className="container flexSB">
                 <div className="left row">
-                    <img src="./images/about.wEBP" alt="" />
+                    {/* <img src="./images/about.wEBP" alt="/" /> */}
+                    <img src={IMGAbout} alt="" />
                 </div>
                 <div className="right row">
                     <Title subtitle="LEARN ANYTHING" title="Benefits About Online Learning Expertise" />
